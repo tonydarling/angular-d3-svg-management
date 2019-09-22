094adfd6e3a1fc062de865b33e13825dc2d4f80f
seniordev-rt96 <seniordev.rt@gmail.com>
2019-09-22 14:10:59 +1000

    deploy

--- src/app/visuals/graph/graph.component.ts
+++ src/app/visuals/graph/graph.component.ts
@@ -19,7 +19,7 @@ export class GraphComponent implements OnInit, AfterViewInit {
   @Input('nodes') nodes;
   @Input('links') links;
   graph: ForceDirectedGraph;
-  private _options: { width, height } = { width: 800, height: 600 };
+  public _options: { width, height } = { width: 800, height: 600 };
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
--- src/app/visuals/shared/link-visual/link-visual.component.ts
+++ src/app/visuals/shared/link-visual/link-visual.component.ts
@@ -6,10 +6,10 @@ import { Link } from '../../../d3';
   template: `
     <svg:line
         class="link"
-        [attr.x1]="link.source.x"
-        [attr.y1]="link.source.y"
-        [attr.x2]="link.target.x"
-        [attr.y2]="link.target.y"
+        [attr.x1]="link.source"
+        [attr.y1]="link.source"
+        [attr.x2]="link.target"
+        [attr.y2]="link.target"
     ></svg:line>
   `,
   styleUrls: ['./link-visual.component.css']
